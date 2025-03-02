062331d867d1d60b25831793e99d389093618fa0
chriskappa <chriskagkelis@yahoo.com>
2025-03-02 23:33:18 +0000

    Added config files

---
+++ src/configs/TableConfig.ts
@@ -0,0 +1,9 @@
+export const TABLE_CONFIG = {
+    headers: [
+        { id: 1, label: "ID" },
+        { id: 2, label: "Date" },
+        { id: 3, label: "Amount" },
+        { id: 4, label: "Merchant" },
+        { id: 5, label: "Category" },
+    ]
+}
---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type TransactionItem = {
+    amount: number,
+    category: string,
+    date: string,
+    id: number,
+    merchant: string
+}
+
+export type TableHeaderItem = { id: number, label: string }
+
+export type TransactionTableProps = { transactions: TransactionItem[] | [], tableHeaders: TableHeaderItem[], noDataMessage?: string }
+
+export type TableSkeletonProps = { length?: number }
+
+export type TableRowsProps = { transactions?: TransactionItem[], noTableRowsMessage?: string };
+
+export type CapitalizeArg = string;
+
+export type ErrorMessageProps = { message?: string };
+
+export type TransactionResponse = { totalTransactions: number, transactions: TransactionItem[] };
+
+export type TableHeadersProps = { tableHeaders: TableHeaderItem[] };
+
+export type TableItemProps = { children: React.ReactNode };
+
+export type NoDataMessageProps = { message: string };
+
+export type CenteredComponentProps = { children: React.ReactNode };
